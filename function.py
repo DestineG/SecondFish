47546973064f6070bddc482cc3a3d60b46be201b
six <cncdre11@outlook.com>
2026-03-21 07:25:20 +0000

    update: step06 反向传播

--- function.py
+++ function.py
@@ -12,13 +12,15 @@ class Function:
         # result of forward()
         y = self.forward(x)
 
-        # result of grad()
-        grad = self.grad(x)
-        input.set_grad(grad)
+        # record the input variable for backward()
+        self.input = input
 
         return Variable(y)
 
     def forward(self, input: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
+    
+    def backward(self, input: np.ndarray) -> np.ndarray:
         raise NotImplementedError()
 
     def grad(self, input: np.ndarray, epsilon=1e-5) -> np.ndarray:
@@ -46,12 +48,34 @@ class Square(Function):
     def forward(self, input: np.ndarray) -> np.ndarray:
         output = input ** 2
         return output
+    
+    def backward(self, gy: np.ndarray) -> np.ndarray:
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
 
 class Exp(Function):
     def forward(self, input: np.ndarray) -> np.ndarray:
         output = np.exp(input)
         return output
 
+    def backward(self, gy: np.ndarray) -> np.ndarray:
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
 if __name__ == "__main__":
-    Square.test()
-    Exp.test()
+    A = Square()
+    B = Exp()
+    C = Square()
+    x = Variable(np.array([0.5, 0.7]))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+    y.set_grad(np.array([1.0, 1.0]))
+    b.set_grad(C.backward(y.get_grad()))
+    a.set_grad(B.backward(b.get_grad()))
+    x.set_grad(A.backward(a.get_grad()))
+    print(f"Input: {x.data}")
+    print(f"Output: {y.data}")
+    print(f"Gradient: {x.get_grad()}")
